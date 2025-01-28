1df8bbb7cccea6b3df34b4a701562cbce2baa4d6
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-01-28 16:28:25 -0300

    add_button_and_text

--- app/src/main/java/com/example/app_encomendei/TrackingActivity.kt
+++ app/src/main/java/com/example/app_encomendei/TrackingActivity.kt
@@ -1,5 +1,6 @@
 package com.example.app_encomendei
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,20 +10,41 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.AbsoluteAlignment
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardCapitalization
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.app_encomendei.ui.theme.App_encomendeiTheme
 
 class TrackingActivity : ComponentActivity() {
@@ -42,29 +64,87 @@ class TrackingActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun HomeScreen(padding: PaddingValues) {
+fun HomeScreen(padding: PaddingValues, modifier: Modifier = Modifier) {
 
     var text by remember { mutableStateOf("") }
+    val context = LocalContext.current
 
-    Box(modifier = Modifier.padding(padding)) {
+    Box(
+        modifier = Modifier
+            .padding(padding)
+    ) {
 
         Column(
-            modifier = Modifier
+            modifier = modifier
                 .fillMaxSize()
+                .padding(0.dp)
                 .background(color = Color.White),
             verticalArrangement = Arrangement.Center,
             horizontalAlignment = Alignment.CenterHorizontally
 
         ) {
-            OutlinedTextField(
-                value = text,
-                onValueChange = { text = it },
-                label = { Text("Digite seu código de rastreio") }
-            )
+            Row(
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                OutlinedTextField(
+                    value = text,
+                    onValueChange = { text = it },
+                    label = { Text("Digite aqui seu código de rastreio") },
+                    placeholder = { Text("EX: AB123456789BR") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(bottom = 70.dp),
+                    textStyle = TextStyle(
+                        color = Color.Black,
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Bold
+                    ),
+                    singleLine = true,
+                    keyboardOptions = KeyboardOptions(
+                        capitalization = KeyboardCapitalization.Characters,
+                        keyboardType = KeyboardType.Text
+                    ),
+                    visualTransformation = VisualTransformation.None,
+                    colors = TextFieldDefaults.outlinedTextFieldColors(
+                        focusedBorderColor = Color(0xFF03A9F4),
+                        unfocusedBorderColor = Color(0xFF260366),
+                        cursorColor = Color(0xFF03A9F4),
+                        focusedLabelColor = Color(0xFF03A9F4),
+                        unfocusedLabelColor = Color.Gray
+                    ),
+                    shape = MaterialTheme.shapes.medium
+                )
+            }
+
+            Row(
+                modifier = Modifier
+                    .padding(16.dp)
+            ) {
+                Button(
+                    modifier = Modifier
+                        .height(50.dp)
+                        .fillMaxWidth(),
+                    shape = RoundedCornerShape(10.dp),
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color.Blue,
+                        contentColor = Color.White,
+                    ),
+                    enabled = true,
+                    onClick = {
+                        val intent = Intent(context, MainActivity::class.java)
+                        context.startActivity(intent)
+                    }
+                ) {
+                    Text(text = "Rastrear")
+                }
+            }
         }
     }
 }
+
 
 @Preview(showBackground = true)
 @Composable
