637fd80942f63dfc8cd765a56f9dd40998f7c822
matheusbento2016@hotmail.com <140640590+Mouthlolses@users.noreply.github.com>
2025-02-01 22:21:10 -0300

    main_alterations

--- app/src/main/java/com/example/app_encomendei/MainActivity.kt
+++ app/src/main/java/com/example/app_encomendei/MainActivity.kt
@@ -25,7 +25,6 @@ import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
